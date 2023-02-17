90ecfd1030fcb025284ce4712286b9c5c3445ebb
Raul <raulmayas20@gmail.com>
2023-02-16 18:32:07 -0600

    Using classes

---
+++ index.js
@@ -0,0 +1,18 @@
+const Forward = require("./lib/Forward");
+const Midfielder = require("./lib/Midfielder");
+const Defender = require("./lib/Defender");
+const Goalkeeper = require("./lib/Goalkeeper");
+
+
+const eHaaland = new Forward(
+  "Erling Haaland",
+  "Norway",
+  "Manchester City",
+  23,
+  "erlinghaaland@hotmail.com",
+  95,
+  92,
+  93
+);
+
+console.log(eHaaland)
